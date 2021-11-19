4342c564f1265c6476c53dd0f90a7d835e3f42a2
Everett Eng <93930051+EverettEng@users.noreply.github.com>
2021-11-19 03:30:25 +0000

    i added a definition for number and removed the spaces between the symbols

--- part2.py
+++ part2.py
@@ -8,8 +8,7 @@ and 10 in the formatted sentence that's already there, and the sum of 99 and the
 number in the formatted sentence that's already there.
 
 '''
-
 #code starts here
-number = input("Enter a number: ")
-print("Ten times your number is", 10 * number)
-print("Ninety-nine more than your number is", 99 + number)
+number = int(input("Enter a number: "))
+print("Ten times your number is", number*10)
+print("Ninety-nine more than your number is", number+99)
